1152f1dceac795bedffc0c20793831ccb3c39521
paskuvan <majopaskuvan@gmail.com>
2018-07-14 17:22:20 -0400

    fonts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'css')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'images')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'fonts')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
